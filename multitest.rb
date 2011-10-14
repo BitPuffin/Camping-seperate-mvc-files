6433438ac258403ddebb0f806c3f668452b9f611
Isak Andersson <MilkshakePanda@live.se>
2011-10-14 22:43:44 +0200

    Fixed not auto updating server issue

--- multitest.rb
+++ multitest.rb
@@ -1,5 +1,5 @@
 Camping.goes :Multitest
 
-require_relative "controllers"
-require_relative "views"
+require_relative "multitest/controllers"
+require_relative "multitest/views"
 #require_relative "models"
